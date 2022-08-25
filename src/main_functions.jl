d1be5f6f0f2419f9080e15b4a535b9c9196284e1
Rodrigo Henriquez <rodrigomha@gmail.com>
2022-08-25 13:35:24 -0700

    model to exponential

--- src/main_functions.jl
+++ src/main_functions.jl
@@ -1,25 +1,34 @@
 include("data/load_data.jl")
 
 function change_load_power!(sys::System, p_change::Float64, power_factor::Float64)
-    load = first(get_components(PowerLoad, sys))
+    load = first(get_components(StaticLoad, sys))
     set_active_power!(load, p_change)
     q_change = p_change * tan(acos(power_factor))
     set_reactive_power!(load, q_change)
 end
 
-function change_load_model!(sys::System, model)
-    load = first(get_components(PowerLoad, sys))
-    set_model!(load, model)
+"""
+Function to change load coefficients of a exponential load of the form:
+P + jQ = P0 * (V / V0)^α + jQ0 * (V / V0)^β
+
+With α = β = 0.0 we get Constant Power load
+With α = β = 1.0 we get Constant Current load
+With α = β = 2.0 we get Constant Impedance load
+"""
+function change_load_coefficients!(sys::System, α::Float64, β::Float64)
+    load = first(get_components(StaticLoad, sys))
+    set_active_power_coefficient(load, α)
+    set_reactive_power_coefficient(load, β)
 end
 
 function attach_3rd_induction_motor!(sys::System)
-    load = first(get_components(PowerLoad, sys))
+    load = first(get_components(StaticLoad, sys))
     ind_motor = Ind_Motor3rd(load)
     add_component!(sys, ind_motor, load)
 end
 
 function attach_5th_induction_motor!(sys::System)
-    load = first(get_components(PowerLoad, sys))
+    load = first(get_components(StaticLoad, sys))
     ind_motor = Ind_Motor(load)
     add_component!(sys, ind_motor, load)
 end
--- src/system_constructor.jl
+++ src/system_constructor.jl
@@ -9,7 +9,23 @@ raw_file = "data/OMIB_Load.raw"
 ##############################
 
 function get_static_system()
-    return System(raw_file)
+    sys = System(raw_file)
+    l = first(get_components(PowerLoad, sys))
+    exp_load = ExponentialLoad(
+        name = get_name(l),
+        available = get_available(l),
+        bus = get_bus(l),
+        active_power = get_active_power(l),
+        reactive_power = get_reactive_power(l),
+        active_power_coefficient = 0.0, # Constant Power
+        reactive_power_coefficient = 0.0, # Constant Power
+        base_power = get_base_power(l),
+        max_active_power = get_max_active_power(l),
+        max_reactive_power = get_max_reactive_power(l),
+    )
+    remove_component!(sys_exp, l)
+    add_component!(sys_exp, exp_load)
+    return sys
 end
 
 function get_genrou_system()
